84802733f01d625fb3ecc13088fe6facc20c4870
VitFly <tommy.babcook@gmail.com>
2021-04-20 18:16:48 +0300

    Modal Window V1.0

--- dist/js/script.js
+++ dist/js/script.js
@@ -48,7 +48,7 @@ document.addEventListener('DOMContentLoaded', () => {
             const t = Date.parse(endtime) - Date.parse(new Date()),
                   days = Math.floor(t /(1000 * 60 * 60 * 24)),
                   hours = Math.floor(t /(1000 * 60 * 60) % 24),
-                  minutes = Math.floor(t /(1000 / 60 ) % 60 ),
+                  minutes = Math.floor(t /(1000 * 60 ) % 60 ),
                   seconds = Math.floor((t /1000) % 60 );
                 
                   return{
@@ -93,5 +93,41 @@ document.addEventListener('DOMContentLoaded', () => {
         }
 
         setClock('.timer', deadline);
+
+        // Modal      // Modal      // Modal      // Modal      // Modal      
+
+        const modalTrigger = document.querySelectorAll('[data-modal]'),
+              modal = document.querySelector('.modal'),
+              modalCloseBtn = document.querySelector('[data-close]');
+
+              modalTrigger.forEach(btn =>{
+                  btn.addEventListener('click', () => {
+                    modal.classList.add('show');
+                    modal.classList.remove('hide');   
+                  // modal.classList.toggle('show');
+                    document.body.style.overflow = 'hidden';              
+                });
+              });
+
+            function closeModal(){
+                modal.classList.add('hide');
+                modal.classList.remove('show');  
+                // modal.classList.toggle('show');
+                document.body.style.overflow = '';    
+            }
+
+              modalCloseBtn.addEventListener('click', closeModal);
+
+            modal.addEventListener('click', (e) => {
+                if (e.target === modal ){
+                    closeModal();
+                }
+            });
+
+            document.addEventListener('keydown', (e) =>{
+                if (e.code === 'Escape' && modal.classList.contains('show')) {
+                    closeModal();
+                }
+            });
 });
 
