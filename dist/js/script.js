b570cf5ea4c8496f9c0521ef7f8f8ee3ef417733
VitFly <tommy.babcook@gmail.com>
2021-04-21 18:50:40 +0300

    rest oper

--- dist/js/script.js
+++ dist/js/script.js
@@ -149,11 +149,12 @@ document.addEventListener('DOMContentLoaded', () => {
 
             // Класс карточек с едой, шаблонизация
             class MenuCard {
-                constructor (src, alt, title, description, price, parentSelector){
+                constructor (src, alt, title, description, price, parentSelector, ...classes){
                     this.src = src;
                     this.alt = alt;
                     this.title = title;
                     this.description = description;
+                    this.classes = classes;
                     this.parent = document.querySelector(parentSelector);
                     this.price = price;
                     this.transfer = 27;
@@ -164,9 +165,16 @@ document.addEventListener('DOMContentLoaded', () => {
                 }
 
                 render(){
+
                     const element = document.createElement('div');
+                        if(this.classes.length === 0){
+                            this.element = 'menu__item';
+                            element.classList.add(this.element);
+                        } else{
+                            this.classes.forEach(className => element.classList.add(className));
+                        }
+                    this.classes.forEach(className => element.classList.add(className));
                     element.innerHTML = `
-                    <div class="menu__item">
                         <img src=${this.src} alt=${this.alt}>
                         <h3 class="menu__item-subtitle">${this.title}</h3>
                         <div class="menu__item-descr">${this.description}</div>
@@ -174,8 +182,7 @@ document.addEventListener('DOMContentLoaded', () => {
                         <div class="menu__item-price">
                             <div class="menu__item-cost">Цена:</div>
                             <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
-                    </div>
-                </div>
+                         </div>
                     `;
                     this.parent.append(element);
                 }
@@ -187,8 +194,9 @@ document.addEventListener('DOMContentLoaded', () => {
                 'Меню "Фитнес"',
                 'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
                 9,
-                '.menu .container'
+                '.menu .container',
              ).render();
+
             //2st Card
              new MenuCard(
                 "img/tabs/elite.jpg",
@@ -196,8 +204,11 @@ document.addEventListener('DOMContentLoaded', () => {
                 'Меню “Премиум“"',
                 'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
                 18,
-                '.menu .container'
+                '.menu .container',
+                'menu__item',
+                'big'
              ).render();
+             
             //s3st Card
              new MenuCard(
                 "img/tabs/post.jpg",
@@ -205,9 +216,12 @@ document.addEventListener('DOMContentLoaded', () => {
                 'Меню "Постное"',
                 'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
                 14,
-                '.menu .container'
+                '.menu .container',
+                'menu__item',
+                'big'
              ).render();
 
+             
 });
 
     
