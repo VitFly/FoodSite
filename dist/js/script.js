2dc6d529d278ae660d01f87fc7d8694487b27128
VitFly <tommy.babcook@gmail.com>
2021-04-21 17:46:16 +0300

    Template card eat

--- dist/js/script.js
+++ dist/js/script.js
@@ -106,7 +106,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 modal.classList.remove('hide');   
               // modal.classList.toggle('show');
                 document.body.style.overflow = 'hidden'; 
-                clearInterval(modalTimerId);
+                clearInterval();
               }
 
               modalTrigger.forEach(btn =>{
@@ -134,16 +134,81 @@ document.addEventListener('DOMContentLoaded', () => {
                 }
             });
 
-            const modalTimerId = setTimeout(openModal, 3000);
+            // const modalTimerId = setTimeout(openModal, 3000);
 
             // Модальное окно вспылвает когда пользователь долистал до конца сайта
             function showModalByScroll(){
-                if(window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight){
+                if(window.pageYOffset + document.documentElement.clientHeight >= document.
+                    documentElement.scrollHeight){
                     openModal();
                     window.removeEventListener('scroll', showModalByScroll);
                 }
             }
 
             window.addEventListener('scroll', showModalByScroll);
+
+            // Класс карточек с едой, шаблонизация
+            class MenuCard {
+                constructor (src, alt, title, description, price, parentSelector){
+                    this.src = src;
+                    this.alt = alt;
+                    this.title = title;
+                    this.description = description;
+                    this.parent = document.querySelector(parentSelector);
+                    this.price = price;
+                    this.transfer = 27;
+                    this.convertToUAH();
+                }
+                convertToUAH(){
+                    this.price = +this.price * this.transfer;
+                }
+
+                render(){
+                    const element = document.createElement('div');
+                    element.innerHTML = `
+                    <div class="menu__item">
+                        <img src=${this.src} alt=${this.alt}>
+                        <h3 class="menu__item-subtitle">${this.title}</h3>
+                        <div class="menu__item-descr">${this.description}</div>
+                        <div class="menu__item-divider"></div>
+                        <div class="menu__item-price">
+                            <div class="menu__item-cost">Цена:</div>
+                            <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+                    </div>
+                </div>
+                    `;
+                    this.parent.append(element);
+                }
+            }
+            //1st Card 
+             new MenuCard(
+                "img/tabs/vegy.jpg",
+                "vegy",
+                'Меню "Фитнес"',
+                'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+                9,
+                '.menu .container'
+             ).render();
+            //2st Card
+             new MenuCard(
+                "img/tabs/elite.jpg",
+                "elite",
+                'Меню “Премиум“"',
+                'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+                18,
+                '.menu .container'
+             ).render();
+            //s3st Card
+             new MenuCard(
+                "img/tabs/post.jpg",
+                "post",
+                'Меню "Постное"',
+                'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
+                14,
+                '.menu .container'
+             ).render();
+
 });
+
+    
 
